5f974c9b102ede9ddcbeba86dfea1fd2a3812182
Krystian <MojtelefonjestMobil1>
2019-08-04 10:23:57 +0200

    Not working

---
+++ src/main/java/pl/krystian/App.java
@@ -0,0 +1,34 @@
+package pl.krystian;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        SettersGetters settersGetters = new SettersGetters();
+        settersGetters.setId(1);
+        settersGetters.setMarka("Ford");
+        settersGetters.setPrzebieg(230000);
+        settersGetters.setRocznik(2004);
+
+        Configuration con = new Configuration().configure().addAnnotatedClass(SettersGetters.class);
+
+        SessionFactory sf = con.buildSessionFactory();
+
+        Session session = sf.openSession();
+
+        Transaction tx = session.beginTransaction();
+
+        session.save(settersGetters);
+
+        tx.commit();
+    }
+}
